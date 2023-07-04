adadf229866b3170032a8307bd2b6fa52cb4d722
MacrossJS <macross.js@mail.ru>
2023-07-04 07:04:28 +0300

    .send_copy(chat_id=message.chat.id)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
+
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 API_TOKEN: str = os.getenv('TG_TOKEN')
 
@@ -12,26 +13,29 @@ dp: Dispatcher = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=["help"]))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
+@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+        print(message.json(indent=4, exclude_none=True))
+    except TypeError:
+        await message.reply(text='Данный тип апдейтов не поддерживается '
+                                 'методом send_copy')
 
-
-# Регистрируем хэндлеры
-dp.message.register(process_start_command, Command(commands=["start"]))
-dp.message.register(process_help_command, Command(commands=['help']))
-dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
